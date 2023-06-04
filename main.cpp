9cff28a5e4954ab084f35155db5906f0b7436044
Ahmad Hassan <43483116+MidoHassan@users.noreply.github.com>
2023-06-04 19:36:26 +0300

    main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    cout<<"hello world";
+    cout<<"######## Circuits solver #############";
+    cout<<"Enter the number of nodes:";
     int n;
     cin>>n;
+
+    int cmnd=0;
+    
+    do{
+        cout<<"Choose action:\n";
+        cout<<"1-Add 2-nodes component\n";
+        cout<<"2-Add single-node component\n";
+        cout<<"3-solve\n";
+        cin>>cmnd;
+
+    }while(cmnd!=3);
+
+
+    //TODO: OUTPUT all VOLTS
+    do{
+        cout<<"Choose what you wanT:\n";
+        cout<<"1-Get current\n";
+        cout<<"2-Get voltage difference\n";
+        cout<<"3-Exit\n";
+        cin>>cmnd;
+
+    }while(cmnd!=3);
+
+    cout<<"Finished\n";
 
 
     return 0;
