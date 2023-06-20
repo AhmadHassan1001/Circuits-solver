fa5e6f41dface645791bc02d9d946bcc07fee508
nasr7322 <120938183+nasr7322@users.noreply.github.com>
2023-06-20 14:48:05 +0300

    starting with a test

--- main.cpp
+++ main.cpp
@@ -7,13 +7,32 @@
 using namespace cv;
 using namespace std;
 
+void getCorners(Mat imgin, Mat imgout) {
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(imgin, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	drawContours(imgout, contours, -1, Scalar(255, 0, 255), 2);
+}
+
 
 int main() {
+
 	string path = "Resources/test.png";
 	Mat img = imread(path);
-	imshow("Image", img);
+	Mat imgout;
+	//resize(img, img,Size(), 0.75, 0.75);
+
+	//Mat imggray, imgcanny, imgblur, imgdil;
+	//cvtColor(img, imggray, COLOR_BGR2GRAY);
+	//GaussianBlur(imggray, imgblur, Size(3, 3), 3, 0);
+	//Canny(imgblur, imgcanny, 25, 75);
+	//Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	//dilate(imgcanny, imgdil, kernel);
+
+	getCorners(img, imgout);
+	imshow("imgout", imgout);
+
 	waitKey(0);
-	
 	return 0;
 }
 
